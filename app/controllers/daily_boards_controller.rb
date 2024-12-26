e4265929d8e22416a83a9f909b3b3abdfdd39a90
kairosb <kairomatheussb@gmail.com>
2024-12-26 07:37:58 -0300

    feat: gerando controller inicial

---
+++ app/controllers/daily_boards_controller.rb
@@ -0,0 +1,49 @@
+class DailyBoardsController < ApplicationController
+  before_action :set_daily_board, only: %i[show update destroy]
+
+  # GET /daily_boards
+  def index
+    @daily_boards = DailyBoard.where(user: current_user)
+    render json: @daily_boards
+  end
+
+  # GET /daily_boards/:id
+  def show
+    render json: @daily_board
+  end
+
+  # POST /daily_boards
+  def create
+    @daily_board = current_user.daily_boards.new(daily_board_params)
+    if @daily_board.save
+      render json: @daily_board, status: :created
+    else
+      render json: { errors: @daily_board.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+
+  # PATCH/PUT /daily_boards/:id
+  def update
+    if @daily_board.update(daily_board_params)
+      render json: @daily_board
+    else
+      render json: { errors: @daily_board.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+
+  # DELETE /daily_boards/:id
+  def destroy
+    @daily_board.destroy
+    head :no_content
+  end
+
+  private
+
+  def set_daily_board
+    @daily_board = current_user.daily_boards.find(params[:id])
+  end
+
+  def daily_board_params
+    params.require(:daily_board).permit(:name, :total_estimate)
+  end
+end
---
+++ test/controllers/daily_boards_controller_test.rb
@@ -0,0 +1,7 @@
+require "test_helper"
+
+class DailyBoardsControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
