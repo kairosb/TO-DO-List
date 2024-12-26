9a330d9abc63fbed64ce9172ad04924e8ac15770
kairosb <kairomatheussb@gmail.com>
2024-12-26 08:54:12 -0300

    feat: adicionando nova tabela para juntar campos do devise

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2024_12_26_103914) do
+ActiveRecord::Schema[8.0].define(version: 2024_12_26_114732) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -86,6 +86,11 @@ ActiveRecord::Schema[8.0].define(version: 2024_12_26_103914) do
     t.string "password"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
   add_foreign_key "board_columns", "daily_boards"
