4a50bc24554ab401c3520c18896d66d72c9a810f
kairosb <kairomatheussb@gmail.com>
2024-12-26 11:14:55 -0300

    feat: tirando necessidade do name no user

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,6 @@ class User < ApplicationRecord
   has_many :todo_lists
   has_many :daily_boards
 
-  validates :name, presence: true
   validates :email, presence: true, uniqueness: true
 
   devise :database_authenticatable, :registerable,
