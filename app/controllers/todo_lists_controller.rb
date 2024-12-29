45a14dee525f925f609c5949a7108a0b0d20906f
kairosb <kairomatheussb@gmail.com>
2024-12-29 00:01:32 -0300

    fix: ajuste no controller

--- app/controllers/todo_lists_controller.rb
+++ app/controllers/todo_lists_controller.rb
@@ -1,16 +1,19 @@
 class TodoListsController < ApplicationController
   before_action :authenticate_user!
-  before_action :set_todo_list, only: [ :show, :update, :destroy ]
+  before_action :set_todo_list, only: %i[ show edit update destroy]
 
   # GET /todo_lists
   def index
     @todo_lists = current_user.todo_lists
-    render json: @todo_lists
   end
 
-  # GET /todo_lists/:id
   def show
-    render json: @todo_list
+    redirect_to todo_lists_path, notice: "A lista foi carregada com sucesso."
+  end
+
+  # GET /todo_lists/new
+  def new
+    @todo_list = TodoList.new
   end
 
   # POST /todo_lists
@@ -18,25 +21,30 @@ class TodoListsController < ApplicationController
     @todo_list = current_user.todo_lists.new(todo_list_params)
 
     if @todo_list.save
-      render json: @todo_list, status: :created
+      redirect_to todo_lists_path, notice: "Lista criada com sucesso!"
     else
-      render json: { errors: @todo_list.errors.full_messages }, status: :unprocessable_entity
+      render :new, status: :unprocessable_entity
     end
+  end
+
+  # GET /todo_lists/:id/edit
+  def edit
+    # `@todo_list` já está definido pelo before_action `set_todo_list`
   end
 
   # PUT/PATCH /todo_lists/:id
   def update
     if @todo_list.update(todo_list_params)
-      render json: @todo_list
+      redirect_to todo_lists_path, notice: "Lista atualizada com sucesso!"
     else
-      render json: { errors: @todo_list.errors.full_messages }, status: :unprocessable_entity
+      render :edit, status: :unprocessable_entity
     end
   end
 
   # DELETE /todo_lists/:id
   def destroy
     @todo_list.destroy
-    head :no_content
+    redirect_to todo_lists_path, notice: "Lista removida com sucesso!"
   end
 
   private
@@ -44,7 +52,7 @@ class TodoListsController < ApplicationController
   def set_todo_list
     @todo_list = current_user.todo_lists.find(params[:id])
   rescue ActiveRecord::RecordNotFound
-    render json: { error: "TodoList not found" }, status: :not_found
+    redirect_to todo_lists_path, alert: "Lista não encontrada."
   end
 
   def todo_list_params
