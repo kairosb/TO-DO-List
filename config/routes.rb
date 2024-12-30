b9cc5bfa3a3bfec78596a1cb34eba472a3212381
kairosb <kairomatheussb@gmail.com>
2024-12-30 18:40:08 -0300

    feat: criacao da nova pagina

--- config/routes.rb
+++ config/routes.rb
@@ -5,9 +5,11 @@ Rails.application.routes.draw do
 
   get "/home", to: "home#index"
 
+  get "/pending_tasks", to: "tasks#pending", as: :pending_tasks
+
   resources :todo_lists do
     resources :tasks, only: %i[index new edit create show update destroy]
-    resource :list_board, only: %i[show create update destroy] do 
+    resource :list_board, only: %i[show create update destroy] do
       resources :board_columns, only: %i[index create update destroy]
       resources :task_assignments, only: %i[create update destroy]
     end
@@ -17,6 +19,6 @@ Rails.application.routes.draw do
     resources :board_columns, only: %i[index new create update destroy]
     resources :task_assignments, only: %i[new create update destroy]
   end
-  
-  resources :task_assignments, only: [:update]
+
+  resources :task_assignments, only: [ :update ]
 end
